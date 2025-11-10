b3a88ecf275dd701169a46c9be3d18ed9faf9112
johantchulacan <johant.chulacan@campusucc.edu.co>
2025-11-10 15:45:11 +0000

    se realizo la clase cuenta

---
+++ proyecto-final/cuenta.java
@@ -0,0 +1,33 @@
+// Clase que representa una cuenta bancaria
+public class cuenta {
+    private int cuentaNumber;
+    private String titularName;
+    private double balance;
+
+    public Cuenta(int cuentaNumber, String titularName, double balance) {
+        this.cuentaNumber = cuentaNumber;
+        this.titularName = titularName;
+        this.balance = balance;
+
+    }
+
+    public int getCuentaNumber() { return CuentaNumber; }
+    public String gettitularName() { return titularName; }
+    public double getBalance() { return balance; }
+
+    public void deposit(double amount) {
+        if (amount > 0) balance += amount;
+    }
+
+    public boolean retirar(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public void displayInfo() {
+        System.out.printf("Cuenta #%d | Titular: %s | Saldo: %.2f%n", cuentaNumber, titularName, balance);
+    }
+}
